2d66fd571f25838fc9c8c6ad58baedb0d186e656
Adrian Fernandez <79542@alumnos.utleon.edu.mx>
2025-09-17 19:37:44 -0600

    Formulario

--- src/app/heroes/heroes-list/heroes-list.component.ts
+++ src/app/heroes/heroes-list/heroes-list.component.ts
@@ -3,9 +3,49 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-list',
   standalone: false,
-  templateUrl: './heroes-list.component.html',
-  styleUrl: './heroes-list.component.css'
+  templateUrl: './heroes-list.component.html'
+
 })
 export class HeroesListComponent {
+
+  imageWidth:number=80;
+  imageMargin:number=2;
+  mostrarImage:boolean=true;
+  listFilter:string='';
+
+  showImages():void{
+    this.mostrarImage=!this.mostrarImage;
+  }
+  
+  heroes:any[]=[
+    {
+      imagen:"https://dragonball-api.com/characters/goku_normal.webp",
+      nombre: "Goku",
+      poder:"Kame Hamen Ha",
+      Raza: "Saiyan",
+      Ki: 9000000000
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/vegeta_normal.webp",
+      nombre: "Vegeta",
+      poder:"Final Flash",
+      Raza: "Saiyan",
+      Ki: 19840000
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/picolo_normal.webp",
+      nombre: "Picolo",
+      poder:"Makankosapo",
+      Raza: "Namekiano",
+      Ki: 50000000
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/Freezer.webp",
+      nombre: "Freezer",
+      poder:"Death Ball",
+      Raza: "Alien",
+      Ki: 9500
+    },
+  ]
 
 }
