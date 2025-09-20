daae7ca21783bdf65c310f253d3a0de11fdc1dfd
Adrian Fernandez <79542@alumnos.utleon.edu.mx>
2025-09-19 18:49:33 -0600

    para casa

---
+++ src/app/formularios/operas-bas/operas-bas.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operas-bas',
+  standalone: false,
+  templateUrl: './operas-bas.component.html',
+  styleUrl: './operas-bas.component.css'
+})
+export class OperasBasComponent {
+
+  num1:string='';
+  num2:string='';
+  resultado:number=0;
+  calculo:string='calculo'
+
+  calcular():void{
+    switch(this.calculo){
+      case 'suma':this.resultado=parseInt(this.num1)+parseInt(this.num2); break;
+      case 'resta':this.resultado=parseInt(this.num1)-parseInt(this.num2); break;
+      case 'mult':this.resultado=parseInt(this.num1)*parseInt(this.num2); break;
+      case 'divi':this.resultado=parseInt(this.num1)/parseInt(this.num2); break;
+    }
+  
+  }
+}
